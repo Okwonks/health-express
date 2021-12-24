a7feacd8d9c8d44984f4094a78128f242ac5ec30
Albert Oketch <albert@paygoenergy.co>
2021-12-24 11:21:35 +0300

    components: generate required detail pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { DoctorProfileComponent } from './doctor-profile/doctor-profile.component';
+import { PatientProfileComponent } from './patient-profile/patient-profile.component';
+import { HospitalComponent } from './hospital/hospital.component';
+import { HospitalDetailsComponent } from './hospital-details/hospital-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserProfileComponent,
+    DoctorProfileComponent,
+    PatientProfileComponent,
+    HospitalComponent,
+    HospitalDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hospital-details/hospital-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hospital-details',
+  templateUrl: './hospital-details.component.html',
+  styleUrls: ['./hospital-details.component.scss']
+})
+export class HospitalDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
